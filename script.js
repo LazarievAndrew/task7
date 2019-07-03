3135473ce897fd1f0efc511a5e182edc233d0e98
LazarievAndrew <lazarev505@gmail.com>
2019-07-03 08:01:08 +0300

    logic

---
+++ script.js
@@ -0,0 +1,14 @@
+var a = ""; 
+var b = 7; 
+var c = null;
+var d = 2; 
+var e;
+
+
+// alert( a || c /*ЛО */ b || d ); // 2
+// alert( e  /*ЛО */  c && b /*ЛО */  d ); //  null
+// alert( e  /*ЛО */  c /*ЛО */ b /*ЛО */ e /*ЛО */ d); // 7
+
+alert( a || c && b || d ); // 2
+alert( e || c && b && d ); //  null
+alert( e || c || b || e && d); // 7
